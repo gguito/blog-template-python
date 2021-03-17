e3f221ea13a31fc25e6e6754e90ae576908ad07b
Guilherme <gui155@hotmail.com>
2021-03-17 12:55:42 -0300

    first commit

---
+++ blog/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage,\
+                                  PageNotAnInteger
+from django.views.generic import ListView
+from .models import Post, Comment
+from .forms import CommentForm
+
+def post_list(request):
+  object_list = Post.published.all()
+  paginator = Paginator(object_list, 3)
+  page = request.GET.get('page')
+  try:
+    posts = paginator.page(page)
+  except PageNotAnInteger:
+    posts = paginator.page(1)
+  except EmptyPage:
+    posts = paginator.page(paginator.num_pages)
+  posts = Post.published.all()
+  return render(request, 'blog/post/list.html',{'page': page, 'posts': posts})
+
+def post_detail(request, year, month, day, post):
+  post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month, publish__day=day)
+  comments = post.comments.filter(active=True)
+  new_comment = None
+  if request.method == 'POST':
+    comment_form = CommentForm(data=request.POST)
+    if comment_form.is_valid():
+      new_comment = comment_form.save(commit=False)
+      new_comment.post = post
+      new_comment.save()
+  else:
+    comment_form = CommentForm()
+  return render(request, 'blog/post/detail.html', {'post': post, 
+                                                    'comments': comments, 
+                                                    'new_comment': new_comment,
+                                                    'comment_form': comment_form})
+
+class PostListView(ListView):
+  queryset = Post.published.all()
+  context_object_name = 'posts'
+  paginate_by = 3
+  template_name = 'blog/post/list.html'
+
